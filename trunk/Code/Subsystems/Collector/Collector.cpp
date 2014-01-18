b85889abdbddc6d3912f4ddde2966d727ef6f65f
matthewhlee96@gmail.com <matthewhlee96@gmail.com@151ac6ff-986a-871b-022e-23d3b99ee23e>
2014-01-18 04:36:44 +0000

    Added Collector class

---
+++ trunk/Code/Subsystems/Collector/Collector.cpp
@@ -0,0 +1,47 @@
+#include "Collector.h"
+
+/* Roflcopter
+ 
+   ROFL:ROFL:LOL:ROFL:ROFL
+              ^
+  L     /------------- 
+  O ====          []  \
+  L     \              \
+         \______________]
+            I      I
+        ----------------/
+
+(This roflcopter is staying, remove it and you will suffer the death sentence)
+ */
+
+Collector::Collector(Victor *motor, Solenoid *piston, Compressor *compressor, DigitalInput *limitSwitch) {
+	m_motor = motor;
+	m_piston = piston;
+	m_compressor = compressor;
+	m_compressor->Start();
+	m_limitSwitch = limitSwitch;
+}
+
+bool Collector::GetLimitSwitch() {
+	return m_limitSwitch->Get();
+}
+
+void Collector::SpinInwards() {
+	m_motor->Set(1.0);
+}
+
+void Collector::SpinOutwards() {
+	m_motor->Set(-1.0);
+}
+
+void Collector::SpinStop() {
+	m_motor->Set(0);
+}
+
+void Collector::PistonUp() {
+	m_piston->Set(true);
+}
+
+void Collector::PistonDown(void) {
+	m_piston->Set(false);
+}
---
+++ trunk/Code/Subsystems/Collector/Collector.h
@@ -0,0 +1,40 @@
+#ifndef COLLECTOR_H
+#define COLLECTOR_H
+
+#include "WPILib.h"
+
+// SAFETY PIG HAS ARRIVED
+//  _._ _..._ .-',     _.._(`))
+// '-. `     '  /-._.-'    ',/
+//    )         \            '.
+//   / _    _    |             \
+//  |  a    a    /              |
+//  \   .-.                     ;  
+//   '-('' ).-'       ,'       ;
+//      '-;           |      .'
+//         \           \    /
+//         | 7  .__  _.-\   \
+//         | |  |  ``/  /`  /
+//        /,_|  |   /,_/   /
+//           /,_/      '`-'
+//
+// This shouldn't be taken out in the final code,
+// If this safety pig is removed, the program will blow up (true story)
+
+class Collector {
+public:
+	Collector(Victor *motor, Solenoid *piston, Compressor *compressor, DigitalInput *limitSwitch);
+	bool GetLimitSwitch();
+	void SpinInwards();
+	void SpinOutwards();
+	void SpinStop();
+	void PistonUp();
+	void PistonDown();
+private:
+	Victor *m_motor;
+	Solenoid *m_piston;
+	Compressor *m_compressor;
+	DigitalInput *m_limitSwitch;
+};
+
+#endif
