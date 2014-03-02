d74554dd8000e53b3c839e703e6f532f935ea372
fern.wilson2@frontier.com <fern.wilson2@frontier.com@151ac6ff-986a-871b-022e-23d3b99ee23e>
2014-03-02 21:21:05 +0000

    This tag is based on testing performed on 28-Feb-2014, where the robot was opened up for a 2 hour period.

---
+++ tags/02-Mar-2014/Code/Subsystems/Shooter/Shooter.cpp
@@ -0,0 +1,111 @@
+#include "Shooter.h"
+
+double shoot_power = 1;
+double reset_power = 0.15;
+double upDownArmTime = 0.25;
+
+/* The shootTime value is very important to understand before changing.  
+ * It controls the time that the shooter arm is allowed to rotate.
+ * If the value is too high, the arm will wrap around (and into) the
+ * back of the robot. 
+ */
+double shootTime = 0.2;
+
+Timer* timer = new Timer();
+
+Shooter::Shooter(Talon *motorLeft1, Talon *motorLeft2, Talon *motorRight1,
+		Talon *motorRight2, DigitalInput *limitSwitch, Collector *collector){
+	m_collector = collector;
+	m_motorLeft1 = motorLeft1;
+	m_motorLeft2 = motorLeft2;
+	m_motorRight1 = motorRight1;
+	m_motorRight2 = motorRight2;
+	m_limitSwitch = limitSwitch;
+	
+}
+
+Shooter::~Shooter (){
+	//empty
+}
+
+void Shooter::Set(double power) {
+	m_motorLeft1->Set(power);
+	m_motorLeft2->Set(power);
+	m_motorRight1->Set(power);
+	m_motorRight2->Set(power);
+}
+
+void Shooter::Shoot (){
+	m_motorLeft1->Set(-shoot_power);
+	m_motorLeft2->Set(-shoot_power);
+	m_motorRight1->Set(-shoot_power);
+	m_motorRight2->Set(-shoot_power);
+	/* Commented out
+	if (m_limitSwitchTop->Get()){
+		m_motorLeft1->Set(0);
+		m_motorLeft2->Set(0);
+		m_motorRight1->Set(0);
+		m_motorRight2->Set(0);
+	}*/
+}
+
+void Shooter::Stop() {
+	m_motorLeft1->Set(0);
+	m_motorLeft2->Set(0);
+	m_motorRight1->Set(0);
+	m_motorRight2->Set(0);
+}
+
+void Shooter::Reset() {
+	m_motorLeft1->Set(reset_power);
+	m_motorLeft2->Set(reset_power);
+	m_motorRight1->Set(reset_power);
+	m_motorRight2->Set(reset_power);
+	/*
+	Wait(shootTime*7.5);
+	m_motorLeft1->Set(0);
+	m_motorLeft2->Set(0);
+	m_motorRight1->Set(0);
+	m_motorRight2->Set(0);*/
+}
+
+bool Shooter::GetLimitSwitch() {
+	return m_limitSwitch->Get();
+}
+
+void Shooter::BringArmDown() {
+	Reset();
+	
+	timer->Start();
+	while(true) {
+		if (m_limitSwitch->Get() || timer->Get() >= 4) {
+			Stop();
+			break;
+		}
+	}
+	timer->Stop();
+	timer->Reset();
+}
+
+void Shooter::ShootWithArm() {
+	m_collector->BringArmDown();
+	BringArmDown();
+	
+	Shoot();
+	Wait(shootTime);
+	Stop();
+	Wait(2);
+	BringArmDown();
+}
+
+void Shooter::ShooterPass(){
+	m_collector->PistonPull();
+	Wait(1.25);
+	m_collector->SpinOutwards();
+	Set(0.2);
+	Wait(0.3);
+	Set(0);
+	Wait(2);
+	m_collector->SpinStop();
+	BringArmDown();
+}
